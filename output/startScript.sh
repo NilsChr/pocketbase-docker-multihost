7eb1b773d3bcf789d0da320b6bee48ba99a7a70d
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2023-04-10 22:57:23 +0200

    added config file

--- output/startScript.sh
+++ output/startScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./apps/app1 serve --http=0.0.0.0:8090 --dir=/app1/pb_data &
+./apps/test1 serve --http=0.0.0.0:8090 --dir=/test1/pb_data &
+./apps/test2 serve --http=0.0.0.0:8091 --dir=/test2/pb_data &
 wait -n
 exit $?
